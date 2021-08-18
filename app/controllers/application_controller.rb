85dd568179b5faea2afae4bce64c7495cc1db31d
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-08-18 19:34:26 -0400

    Adds basic user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::API
 
   private
 
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
+
   def not_authorized
-    render json: { error: 'Not Authorized' }, status: :unauthorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
      # routes go here
     end
   end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 end
