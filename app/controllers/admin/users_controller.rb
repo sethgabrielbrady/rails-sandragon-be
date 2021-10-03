345b5531a6895b99370717ffd458ed1929910b6c
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-10-02 20:33:29 -0500

    Clean up and revert

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,9 @@
 class Admin::UsersController < ApplicationController
   before_action :authorize_access_request!
+  ROLES = %w[admin].freeze
 
   def index
     @users = User.all
-    # render json: @users
     render json: @users.as_json(only: [:id, :email, :role])
   end
 
@@ -13,4 +13,18 @@ class Admin::UsersController < ApplicationController
       verify_aud: true
     }
   end
+
+  private
+
+    def allowed_aud
+      action_name == 'update' ? EDIT_ROLES : VIEW_ROLES
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:role)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # include ActiveModel::Serializers::JSON
+  include ActiveModel::Serializers::JSON
   has_secure_password
 
-  enum role: %i[user manager admin].freeze
+  enum role: %i[user admin].freeze
 
   validates :email,
             format: { with: URI::MailTo::EMAIL_REGEXP },
