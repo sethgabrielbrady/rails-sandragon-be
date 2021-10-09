2ea64dd26daddc3386e5fd2a3ee2fa185845f374
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-10-09 17:14:04 -0400

    Adds password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,21 @@
 class ApplicationController < ActionController::API
   include JWTSessions::RailsAuthorization
+  rescue_from ActionController::ParameterMissing, with: :bad_request
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
   rescue_from JWTSessions::Errors::ClaimsVerification, with: :forbidden
   rescue_from JWTSessions::Errors::Expired, with: :expired
+  rescue_from ResetPasswordError, with: :not_authorized
 
   private
 
   def current_user
     @current_user ||= User.find(payload['user_id'])
+  end
+
+  def bad_request
+    render json: { error: 'Bad request' }, status: :bad_request
   end
 
   def not_authorized
@@ -20,5 +28,13 @@ class ApplicationController < ActionController::API
 
   def expired
     render json: { error: 'Expired' }, status: :forbidden
+  end
+
+  def not_found
+    render json: { error: 'Not found' }, status: :not_found
+  end
+
+  def unprocessable_entity(exception)
+    render json: { error: exception.record.errors.full_messages.join(' ') }, status: :unprocessable_entity
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,23 @@ class User < ApplicationRecord
             presence: true,
             uniqueness: { case_sensitive: false }
 
+  def attributes
+    { id: id, email: email, role: role }
+  end
+
+  def generate_password_token!
+    begin
+      self.reset_password_token = SecureRandom.urlsafe_base64
+    end while User.exists?(reset_password_token: self.reset_password_token)
+    # lower this time
+    self.reset_password_token_expires_at = 1.day.from_now
+    save!
+  end
+
+  def clear_password_token!
+    self.reset_password_token = nil
+    self.reset_password_token_expires_at = nil
+    save!
+  end
+
 end
