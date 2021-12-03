b48f8df94d34d44d43e250f58f7bfe7e620a6d1d
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-12-02 23:51:37 -0500

    Fixes posts

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,10 +1,15 @@
 class Material < ApplicationRecord
   include Rails.application.routes.url_helpers
   has_one_attached :image
+  # has_many_attached :files
 
   def image_url
     image.attached? ? url_for(image) : nil
   end
+
+  # def file_url
+  #   file.attached? ? url_for(file) : nil
+  # end
 
   def falsify_any_active
     if self.active
